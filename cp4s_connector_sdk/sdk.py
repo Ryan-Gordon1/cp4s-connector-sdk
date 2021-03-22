144e5606c4939073d315500514ef1b89245a687b
Ryan Gordon <Ryan.Gordon1@ibm.com>
2021-03-22 15:21:41 +0000

    Add parsers to sdk runner and navigational logic

--- cp4s_connector_sdk/sdk.py
+++ cp4s_connector_sdk/sdk.py
@@ -65,3 +65,54 @@ def get_main_app_sub_parser(parent_parser):
 
     return sub_parser
 
+
+def main():
+    """
+    Main entry point for cp4s-connector-sdk
+    """
+
+    # Get main parser object
+    parser = get_main_app_parser()
+
+    # Get sub_parser object, its dest is cmd
+    sub_parser = get_main_app_sub_parser(parser)
+
+    # Add any subcommands to main app parser here
+    cmd_codegen = ConnectorCodegenCmd(sub_parser)
+
+    try:
+        # Parse the arguments
+        args = parser.parse_args()
+
+        if args.cmd is None:
+            parser.print_help()
+            sys.exit()
+
+    except SDKException as err:
+        # Get main_cmd (codegen, docgen etc.)
+        main_cmd = sdk_helpers.get_main_cmd()
+
+        LOG.error(err)
+        LOG.info("\n-----------------\n")
+
+        # Print specifc usage for that cmd for these errors
+        if "too few arguments" in err.message or "no subcommand provided" in err.message:
+            if main_cmd == cmd_codegen.CMD_NAME:
+                cmd_codegen.parser.print_usage()
+            else:
+                parser.print_help()
+
+        # Exit
+        sys.exit()
+
+    # If -v was specified, set the log level to DEBUG
+    if args.verbose:
+        LOG.setLevel(logging.DEBUG)
+        LOG.debug("Logging set to DEBUG mode")
+
+    if args.cmd == cmd_codegen.CMD_NAME:
+        cmd_codegen.execute_command(args)
+
+
+if __name__ == "__main__":
+    main()
