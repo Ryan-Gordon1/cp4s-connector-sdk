cf06acb29b41e1e4b1adc4fb1dd0b960e020d51f
Ryan Gordon <Ryan.Gordon1@ibm.com>
2021-03-22 13:42:40 +0000

    Codegenerating a CAR Connector

--- cp4s_connector_sdk/cmds/codegen_connector.py
+++ cp4s_connector_sdk/cmds/codegen_connector.py
@@ -23,9 +23,9 @@ class ConnectorCodegenCmd(BaseCmd):
     CMD_NAME = "codegen"
     CMD_HELP = "Generate boilerplate code to start developing a connector"
     CMD_USAGE = """
-    $ cp4s-connector-sdk codegen -p <package_name>
-    $ cp4s-connector-sdk codegen -p <package_name> -t CAR
-    $ cp4s-connector-sdk codegen -p <package_name> --connectortype UDI
+    $ connector-sdk codegen -p <package_name>
+    $ connector-sdk codegen -p <package_name> -t CAR
+    $ connector-sdk codegen -p <package_name> --connectortype UDI
     """
     CMD_ADD_PARSERS = ["io_parser"]
 
@@ -55,19 +55,76 @@ class ConnectorCodegenCmd(BaseCmd):
             ###
             # Perform checks
             ###
+            if os.path.exists(args.package):
+                raise SDKException(
+                    u"'{0}' already exists. Add --reload flag to regenerate it".format(args.package))
+
+            if not sdk_helpers.is_valid_package_name(args.package):
+                raise SDKException(
+                    u"'{0}' is not a valid package name".format(args.package))
 
             ###
             # Gather data needed for generation
             ###
+            # The package_name will be specified in the args
+            package_name = args.package
 
+            # Get output_base, use args.output if defined, else current directory
+            output_base = args.output if args.output else os.curdir
+            output_base = os.path.join(
+                os.path.abspath(output_base), package_name)
+
+            jinja_data = {
+                "package_name": package_name
+            }
+
+            # If the output_base directory does not exist, create it
+            if not os.path.exists(output_base):
+                os.makedirs(output_base)
+
+            from jinja2 import Environment, PackageLoader
+            jinja_env = Environment(
+                # Loads Jinja Templates in cp4s_connector_sdk/<<relative_path_to_templates>>
+                loader=PackageLoader("cp4s_connector_sdk", get_jinja_env_location(
+                    connecter_type=args.connectertype)),
+                trim_blocks=True,  # First newline after a block is removed
+                # Leading spaces and tabs are stripped from the start of a line to a block
+                lstrip_blocks=True,
+                keep_trailing_newline=True  # Preserve the trailing newline when rendering templates
+            )
+            # Add custom filters to our jinja_env
+            sdk_helpers.add_filters_to_jinja_env(jinja_env)
+
+            # Assign one of the mapping dict function to connector_mapping_collector so that below we call one thing
+            # but here we define which thing will be called
+            # TODO: Review
+            connector_mapping_collector = get_car_connector_mapping_dict
+            # Prepare a mapping dict based on type of connector
+            # This dict maps our package file structure to  Jinja2 templates
+            # TODO: Refactor as there can be a UDI or CAR package_mapping_dict
+            package_mapping_dict = connector_mapping_collector(jinja_data)
 
             ###
             # Generate a connector package using jinja
             ###
+            newly_generated_files, skipped_files = CmdCodegen.render_jinja_mapping(
+                jinja_mapping_dict=package_mapping_dict,
+                jinja_env=jinja_env,
+                target_dir=output_base,
+                package_dir=output_base)
 
             ###
             # Report the files that were and were not built
             ###
-            
+            if newly_generated_files:
+                LOG.debug("Newly generated files:\n\t> %s",
+                          "\n\t> ".join(newly_generated_files))
+
+            if skipped_files:
+                LOG.debug("Files Skipped:\n\t> %s",
+                          "\n\t> ".join(skipped_files))
+
+            LOG.info("%s %s" %
+                     ("Codegen run finished for ", package_name))
         else:
             self.parser.print_help()
